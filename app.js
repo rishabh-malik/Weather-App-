65a792681ec42c39b05f1d6b983c2a7784016087
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-28 14:49:25 +0530

    displaying latitude and long

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
-//used to request the google maps api
+//used to request the http in this case google maps api
 request(
     {url:'http://maps.googleapis.com/maps/api/geocode/json?address=%20wz%2098%20ram%20garh%20colony%20new%20delhi',
      json:true
 },(error,response,body)=>{
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Address: ${body.results[0].geometry.location.lng}`);
 });

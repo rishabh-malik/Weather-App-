ec7268f88a8b7a29b40add1dd59d5dccee307216
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-28 14:32:33 +0530

    printing object

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 //used to request the google maps api
 request(
-    {url:'http://maps.googleapis.com/maps/api/geocode/json',
+    {url:'http://maps.googleapis.com/maps/api/geocode/json?address=%20wz%2098%20ram%20garh%20colony%20new%20delhi',
      json:true
 },(error,response,body)=>{
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });

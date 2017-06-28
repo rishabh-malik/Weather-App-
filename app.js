1b36f8173336be45259ac737a3e2a58aba06dcdb
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-28 14:56:44 +0530

    user input

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const request = require('request');
+const yargs= require('yargs')
+
+const argv=yargs
+.options({
+    a:{
+        demand:true,
+        alias:'address',
+        describe:'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+
+console.log(argv);
+
 //used to request the http in this case google maps api
 request(
     {url:'http://maps.googleapis.com/maps/api/geocode/json?address=%20wz%2098%20ram%20garh%20colony%20new%20delhi',

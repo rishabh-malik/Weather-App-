627e0cc6264b4aadb78418c460cb00e7f6cd3302
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-28 15:04:27 +0530

    encoding user input

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const yargs= require('yargs')
 
+//taking input
 const argv=yargs
 .options({
     a:{
@@ -14,11 +15,12 @@ const argv=yargs
 .alias('help','h')
 .argv;
 
-console.log(argv);
+//encoding the user input so that it can be send for request
+var encodedAddress= encodeURIComponent(argv.address);
 
 //used to request the http in this case google maps api
 request(
-    {url:'http://maps.googleapis.com/maps/api/geocode/json?address=%20wz%2098%20ram%20garh%20colony%20new%20delhi',
+    {url:`http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
      json:true
 },(error,response,body)=>{
   console.log(`Address: ${body.results[0].formatted_address}`);

d65fc779bf774c4768cae9d29e507a570e5af6b1
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-28 15:23:14 +0530

    error handling

--- app.js
+++ app.js
@@ -23,7 +23,12 @@ request(
     {url:`http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
      json:true
 },(error,response,body)=>{
+  if  (error){
+      console.log('Unable to connect to google servers');
+  }else if(body.status === 'ZERO_RESULTS'){
+      console.log('Unable to find address.');
+  }else{
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Address: ${body.results[0].geometry.location.lng}`);
-});
+}});
